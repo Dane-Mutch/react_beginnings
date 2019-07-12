6fc98ef7846e3cef88c9feb7a72cadf803c5dbf2
Dane <dane_mutch-92@hotmail.com>
2019-07-12 16:26:39 +0100

    initial commit

---
+++ src/components/counter.jsx
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+
+class Counter extends Component {
+    state = { 
+        count: 0,
+        tags: ['tag1', 'tag2', 'tag3']
+    }
+
+    render() {
+        return ( 
+            <div>
+                <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+                <button
+                    onClick={() => this.handleIncrement({id: 1})} 
+                    className ="btn btn-secondary btn-sm">
+                Increment
+                </button>
+                {this.state.tags.length === 0 && "Please create a new tag!"}
+                {this.renderTags()}
+            </div>   
+        );
+    }
+
+    renderTags() { 
+        if (this.state.tags.length === 0) {
+            return <p>There are no tags</p>
+        }
+
+        return <ul>{ this.state.tags.map(tag => <li key={tag}>{tag}</li>)}</ul>
+    }
+
+    getBadgeClasses() {
+        let classes = "badge m-2 badge-";
+        classes += this.state.count === 0 ? "warning" : "primary";
+        return classes;
+    }
+
+    formatCount() { 
+        const { count } = this.state;
+        return count === 0 ? 'Zero' : count;
+    }
+
+    handleIncrement = product => {
+        console.log(product)
+        this.setState({ count: this.state.count + 1 })
+        console.log("Increment clicked", this);
+    }
+}
+ 
+export default Counter;
