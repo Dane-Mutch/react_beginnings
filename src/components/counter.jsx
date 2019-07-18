0c5ceb631c5ad2022d03206f6c7d033548053443
Dane <dane_mutch-92@hotmail.com>
2019-07-18 13:38:52 +0100

    lil commity boi

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,8 +2,7 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
     state = { 
-        count: 0,
-        tags: ['tag1', 'tag2', 'tag3']
+        value: this.props.value,
     }
 
     render() {
@@ -11,22 +10,16 @@ class Counter extends Component {
             <div>
                 <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
                 <button
-                    onClick={() => this.handleIncrement({id: 1})} 
+                    onClick={this.handleIncrement}
                     className ="btn btn-secondary btn-sm">
                 Increment
                 </button>
-                {this.state.tags.length === 0 && "Please create a new tag!"}
-                {this.renderTags()}
+                <button onClick={() => this.props.onDelete(this.state.id)}
+                        className="btn btn-danger btn-sm m-2">
+                        Delete
+                </button>
             </div>   
         );
-    }
-
-    renderTags() { 
-        if (this.state.tags.length === 0) {
-            return <p>There are no tags</p>
-        }
-
-        return <ul>{ this.state.tags.map(tag => <li key={tag}>{tag}</li>)}</ul>
     }
 
     getBadgeClasses() {
@@ -36,15 +29,15 @@ class Counter extends Component {
     }
 
     formatCount() { 
-        const { count } = this.state;
-        return count === 0 ? 'Zero' : count;
+        const { value } = this.state;
+        return value === 0 ? 'Zero' : value;
     }
 
-    handleIncrement = product => {
-        console.log(product)
-        this.setState({ count: this.state.count + 1 })
-        console.log("Increment clicked", this);
+    handleIncrement = () => {
+        this.setState({ value: this.state.value + 1 })
     }
+
+    
 }
  
 export default Counter;
